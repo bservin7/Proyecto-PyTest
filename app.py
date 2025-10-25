17e9ea6b83b172529409e1c6bd5a51d265b2aa78
bservin7 <bservin7@gmail.com>
2025-10-24 19:49:01 -0600

    Probar ejecución automática del CI

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ def saludo():
 
 if __name__ == "__main__":
     print(saludo())
+
+
+def salida():
+    # Prueba de ejecución automática de GitHub Actions
+    return "Adios Mundo"
+
+if __name__ == "__main__":
+    print(salida())
